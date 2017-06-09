e900b6be5dbc0e3e7a5be8f402eb637d2cb37b64
Ben Clark <hello@clarkben.com>
2017-06-09 11:37:15 +0100

    simplifies server to not use ejs view engine. now simple html with express static middleware

--- server.js
+++ server.js
@@ -5,18 +5,14 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+// set the home page route
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
-// set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
-});
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
